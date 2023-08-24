08ffd452d40c058e19e2ec991ba1167f595f811e
cebolapipoca <alexpiedade20@gmail.com>
2023-08-24 19:50:24 -0300

    transição do html para php e foreach de produtos

---
+++ index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ragazzo</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+    <?php 
+
+        $Produtos = [
+        
+            $Produto = 
+            [
+                'nome' => "MENU INDIVIDUAL 1 - ENTRADA + PRATO + BEBIDA",
+                'preco' => "39,90",
+                'imagem' => "imagens/produtos/produto1.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "MENU INDIVIDUAL 2 - ENTRADA + PRATO + BEBIDA",
+                'preco' => "49,90",
+                'imagem' => "imagens/produtos/produto2.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "MENU INDIVIDUAL 3 - ENTRADA + PRATO + BEBIDA",
+                'preco' => "59,90",
+                'imagem' => "imagens/produtos/produto3.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "COXINHA DE FRANGO COM CREMELY",
+                'preco' => "3,49",
+                'imagem' => "imagens/produtos/produto4.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "COXINHA DE CALABRESA",
+                'preco' => "3,49",
+                'imagem' => "imagens/produtos/produto5.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "COXINHA DE 4 QUEIJOS",
+                'preco' => "3,49",
+                'imagem' => "imagens/produtos/produto6.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "60 COXINHAS CLÁSSICAS",
+                'preco' => "119,90",
+                'imagem' => "imagens/produtos/produto7.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "40 COXINHAS CLÁSSICAS",
+                'preco' => "87,90",
+                'imagem' => "imagens/produtos/produto8.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "20 COXINHAS CLÁSSICAS",
+                'preco' => "43,90",
+                'imagem' => "imagens/produtos/produto9.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "10 COXINHAS CLÁSSICAS",
+                'preco' => "26,90",
+                'imagem' => "imagens/produtos/produto10.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "100 MINICOXINHAS CLÁSSICAS",
+                'preco' => "89",
+                'imagem' => "imagens/produtos/produto11.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "50 MINICOXINHAS CLÁSSICAS",
+                'preco' => "44,90",
+                'imagem' => "imagens/produtos/produto12.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "25 MINICOXINHAS CLÁSSICAS",
+                'preco' => "26,90",
+                'imagem' => "imagens/produtos/produto13.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "MINICHURROS DE DOCE DE LEITE",
+                'preco' => "1,98",
+                'imagem' => "imagens/produtos/produto14.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "SUCO DE LIMÃO 300ML",
+                'preco' => "9,90",
+                'imagem' => "imagens/produtos/produto15.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "SUCO DE MORANGO 300ML",
+                'preco' => "9,90",
+                'imagem' => "imagens/produtos/produto16.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "SUCO DE MARACUJÁ 300ML",
+                'preco' => "9,90",
+                'imagem' => "imagens/produtos/produto17.png"
+            ],
+
+            $Produto = 
+            [
+                'nome' => "SUCO DE LARANJA 1L",
+                'preco' => "17,90",
+                'imagem' => "imagens/produtos/produto18.png"
+            ],
+        ];
+    ?>
+    
+    <header>
+        <div class="header-content" id="content-menu-titulo">
+            <img src="imagens/menu-ragazzo.png" alt="" class="ragazzo-header-menu">
+            <img src="imagens/ragazzo_logo.png" alt="" class="ragazzo-titulo">
+        </div>
+        
+        <div class="header-content" id="content-icons">
+            <img src="imagens/ragazzo_lupa.png" alt="" class="icon">
+            <img src="imagens/ragazzo_login.png" alt="" class="icon">
+            <img src="imagens/ragazzo_carrinho.png" alt="" class="icon">
+        </div>
+    </header>
+
+    <div class="ragazzo-slide-carrossel">
+        <picture>
+        <source media="(min-width: 800px )" srcset="imagens/Ragazzo_slide1_1024.png">
+        <img src="imagens/Ragazzo_slide1.png" alt="" srcset="" >
+        </picture>
+    </div>
+
+    <aside>
+        <li class="verde">frete grátis</li>
+        <li class="vermelho">combos</li>
+        <li class="amarelo">pizzas</li>
+        <li class="verde">massas</li>
+        <li class="vermelho">pratos</li>
+        <li class="amarelo">coxinhas</li>
+        <li class="verde">salgados</li>
+        <li class="vermelho">sobremesas</li>
+        <li class="amarelo">bebidas</li>
+        <li class="verde">kit ragazzo</li>
+        <li class="vermelho">congelados</li>
+    </aside>
+
+    
+    <main>
+    <?php foreach ($Produtos as $Produto) { ?>
+        <div class="produto">
+                <div class="produto-text1024">
+                    <h2><?php echo $Produto['nome']?></h2>
+                     <div class="produto-text1024-preco">
+                        <h3>R$</h3>
+                        <h1><?php echo $Produto['preco']?></h1>
+                     </div>
+                </div>
+
+                <div class="produto-modal">
+                    <div class="produto-modal-text1">
+                        <h3><?php echo $Produto['nome']?></h3>
+                    </div>
+
+                    <button>VERIFICAR DISPONIBILIDADE</button>
+
+                    <div class="produto-modal-preco">
+                        <h3>Total:</h3>
+                        <h2>R$</h2>
+                        <h1><?php echo $Produto['preco']?></h1>
+                    </div>
+                </div>
+
+                <div class="heart-icon">
+                    <img src="imagens/coracaonew.png" alt="">
+                </div>
+
+
+            <div class="produto-imagem">
+                <div class="produto-imagem-texto">
+                    <h3>Menu <br> Individual 2- <br> Entrada + <br> Prato + Bebida <br></h3>
+                    <div class="preco"><h2><?php echo $Produto['preco']?></h2></div>
+                </div>
+                <img src="<?php echo $Produto['imagem']?>" alt="">
+            </div>
+
+            <div class="produto-verificar">
+                <button class="">VERIFICAR DISPONIBILIDADE</button>
+                <div class="produto-verificar-preco"><h2>TOTAl: <h1><?php echo $Produto['preco']?></h1</h2></div>
+            </div>
+            
+        </div> 
+        <?php } ?>
+    </main>
+    
+
+<footer>
+        <div class="footer-content1">
+            <div class="footer-content1-text">
+                <h3>Fique por dentro</h3>
+                <h4>receba lançamentos e promoções</h4>
+            </div>
+
+            <div class="footer-input">
+                <input type="email" name="" id="" >
+                <button>me cadastre!</button>
+            </div>
+
+            <div class="footer-content1-text">
+                <h2>Siga o Ragazzo</h2>
+
+                <div class="footer-icons" >
+                    <img src="imagens/2959749_facebook_icon.png" alt="">
+                    <img src="imagens/2959740_bird_news_tweet_twitter_icon.png" alt="">
+                    <img src="imagens/2959742_broadcast_google_streaming_video_youtube_icon.png" alt="">
+                    <img src="imagens/2959747_employment_business_linkedin_work_icon.png" alt="">
+                    <img src="imagens/2959748_instagram_photo_share_icon.png" alt="">
+                    <img src="imagens/whatsapp icon.png" alt="">
+                </div>
+            </div>
+        </div>
+</footer>
+
+    <div class="footer-content2">
+        <h2>Fale com nossa central de atendimento, deixe sua opinião
+            e tire dúvidas de um jeito mais simples e rápido:
+        </h2>
+
+        <div class="footer-content2-atendimento">
+            <div class="atendimento-capitais">
+                <h3>Capitais</h3>
+                <h1>  5698 8000</h1>
+            </div>
+
+            <div class="atendimento-outros">
+                <h3>Demais Localidades</h3>
+                <h1>4003 9093</h1>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer_desktop">
+        <div class="footer_desktop_content1">
+            <h2>Fale com nossa central de atendimento, deixe sua opinião
+                e tire dúvidas de um jeito mais simples e rápido:
+            </h2>
+
+            <div class="footer_desktop_atendimento">
+                <div class="footer_desktop_atendimento_phone" id="capitais">
+                    <h2>Capitais</h2>
+                    <h3>5698 8000</h3>
+                </div>
+
+                <div class="footer_desktop_atendimento_phone">
+                    <h2 class="DemaisLocalidades">Demais Localidades</h2>
+                    <h3>40003 9093</h3>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="footer_desktop_content2"> 
+        <ul>
+            <div><h1>Produtos</h1></div>
+
+            <li>Frete Grátis</li>
+            <li>Combos</li>
+            <li>Pizzas</li>
+            <li>Massas</li>
+            <li>Pratos</li>
+            <li>Coxinhas</li>
+            <li>Salgados</li>
+            <li>Sobremesas</li>
+            <li>Bebidas</li>
+            <li>Kit Ragazzo</li>
+        </ul>
+
+        <ul>
+            <div><h1>O Ragazzo</h1></div>
+
+            <li>SAC RAGAZZO(SAC)</li>
+            <li>Regras do Delivery</li>
+            <li>termos de uso e políticas de privacidade</li>
+            <li>Termos e condições</li>
+            <li>Tablea Nutricional Ragazzo</li>
+            <li>Tabela de Alergênicos Ragazzo</li>
+            <li>Tabela Nutricional Ragazzo Express</li>
+            <li>Tabela de Alergênicos Ragazzo Express</li>
+            <li>Faça sua festa no Ragazzo</li>
+        </ul>
+        
+        <div class="footer_desktop_content2_email">
+            <h1>Fique por dentro</h1>
+            <h2>receba lançamentos e promoções</h2>
+             <div><input type="email" name="" id=""> <button></button></div>
+
+             <div>
+                <div>Siga o Ragazzo</div>
+
+                <div>
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                </div>
+             </div>
+        </div>
+    </div>
+
+<link rel="stylesheet" href="index.js">
+</body>
+</html>
